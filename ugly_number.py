b1ca0f8a220ca8858cb723e9a4f738d045d4c901
Sarah Mahvy <sarah.mahvy.01@gmail.com>
2026-09-09 19:25:13 +0530

    added a condition for if input is 0

--- ugly_number.py
+++ ugly_number.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def isUgly(n:int) -> bool:
-    if n%2 == 0 or n%3 == 0 or n%5 == 0:
+    if n != 0 and(n%2 == 0 or n%3 == 0 or n%5 == 0):
         while n%2 == 0:
             n = n//2
         while n%3 == 0:
@@ -19,4 +19,5 @@ def isUgly(n:int) -> bool:
 print(isUgly(6))   
 print(isUgly(1))
 print(isUgly(14)) #should return false
+print(isUgly(0))
 

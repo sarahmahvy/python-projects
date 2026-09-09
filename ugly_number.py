11f56c8b3ec87d197d413a98c0a8d0d877afe84f
Sarah Mahvy <sarah.mahvy.01@gmail.com>
2026-09-09 19:09:20 +0530

    added more conditional statements to filter out the prime numbers. test case 3 gives desired output.

--- ugly_number.py
+++ ugly_number.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 def isUgly(n:int) -> bool:
     if n%2 == 0 or n%3 == 0 or n%5 == 0:
-        return True
+        while n%2 == 0:
+            n = n//2
+        while n%3 == 0:
+            n = n//3
+        while n%5 == 0:
+            n = n//5
+        if n == 1:
+            return True
+        else:
+            return False
     elif n == 1:
         return True
     else:

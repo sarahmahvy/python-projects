665182ce9bcacb0d924437545561e58e6da4a008
Sarah Mahvy <sarah.mahvy.01@gmail.com>
2026-09-15 17:35:15 +0530

    Added responsiveness in the longest common prefix function.
    User can now enter their own list of strings to find the longest common prefix.
    Also added a message to indicate when no common prefix is found.

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -2,6 +2,7 @@
 
 def longestcommonprefix(strs:List[str]):
     prefix = ""
+    strs = [s.lower() for s in strs]
     for i in range(len(strs[0])):
         char = strs[0][i]
         for j in range(1, len(strs)):
@@ -10,6 +11,16 @@ def longestcommonprefix(strs:List[str]):
         prefix += char
     return prefix
 
-print(longestcommonprefix(["flower","flow","flight"]))  # Output: "fl"
-print(longestcommonprefix(["dog","racecar","car"]))  # Output: ""
-print(longestcommonprefix(["interspecies","interstellar","interstate"]))  # Output: "inters"
+response = True
+while response:
+    strs = list(map(str, input("Enter the list of strings separated by a space to find the longest common prefix: ").split()))
+    if strs == []:
+        print("Please enter a valid list of strings. (press enter to continue)")
+        continue
+    if longestcommonprefix(strs) == "":
+        print("There is no common prefix in the given list of strings.")
+    else:
+        print(longestcommonprefix(strs))
+    response = input("Do you want to continue? (y/n): ")
+    if response.lower() == 'n':
+        response = False
